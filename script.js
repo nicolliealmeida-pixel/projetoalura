63b6754cd7f0596f3de347d3a5f99e9d569d9d3b
nicolliealmeida-pixel <nicollie.almeida@escola.pr.gov.br>
2026-09-05 17:44:51 -0300

    xcz

--- script.js
+++ script.js
@@ -27,3 +27,58 @@ botoes.forEach(function (botao) {
     });
 
 });
+// BOTÕES DE CURTIDA
+
+const botoes = document.querySelectorAll(".botao");
+
+botoes.forEach(function (botao) {
+
+    let curtiu = false;
+
+    botao.addEventListener("click", function () {
+
+        console.log("fui clicado");
+
+        let texto = botao.querySelector("span");
+
+        if (curtiu === false) {
+
+            texto.textContent++;
+
+            curtiu = true;
+
+        } else {
+
+            texto.textContent--;
+
+            curtiu = false;
+
+        }
+
+    });
+
+});
+
+
+// BOTÃO DE ALTERAR O TEMA
+
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+
+btnTemaEscuro.addEventListener("click", mudaTema);
+
+
+function mudaTema() {
+
+    const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+
+        corpoPagina.classList.remove("tema-escuro");
+
+    } else {
+
+        corpoPagina.classList.add("tema-escuro");
+
+    }
+
+}
