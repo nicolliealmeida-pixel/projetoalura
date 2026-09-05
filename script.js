6902b53168cf8d4445330b0558d13f3544991717
nicolliealmeida-pixel <nicollie.almeida@escola.pr.gov.br>
2026-09-05 17:41:53 -0300

    xx

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const botoes = document.querySelectorAll(".botao");
 
-botoes.forEach(function (botao) {
-    let curtiu = false;
-
-    botao.addEventListener("click", function () {
-
-        let texto = botao.querySelector("span");
-
-        if (curtiu === false) {
-            texto.textContent++;
-            curtiu = true;
-        } else {
-            texto.textContent--;
-            curtiu = false;
-        }
-
-    });
-});
 const botoes = document.querySelectorAll(".botao");
 
 botoes.forEach(function (botao) {
