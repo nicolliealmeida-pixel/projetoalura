6dec379960fdcdd74df09f32d3adda9bf4bcb858
nicolliealmeida-pixel <nicollie.almeida@escola.pr.gov.br>
2026-09-05 17:56:24 -0300

    ...

--- script.js
+++ script.js
@@ -6,61 +6,20 @@ botoes.forEach(function (botao) {
 
     botao.addEventListener("click", function () {
 
-        console.log("fui clicado");
-
         let texto = botao.querySelector("span");
 
         if (curtiu === false) {
-
             texto.textContent++;
-
             curtiu = true;
-
         } else {
-
             texto.textContent--;
-
             curtiu = false;
-
-        }
-
-    });
-
-});
-// BOTÕES DE CURTIDA
-
-const botoes = document.querySelectorAll(".botao");
-
-botoes.forEach(function (botao) {
-
-    let curtiu = false;
-
-    botao.addEventListener("click", function () {
-
-        console.log("fui clicado");
-
-        let texto = botao.querySelector("span");
-
-        if (curtiu === false) {
-
-            texto.textContent++;
-
-            curtiu = true;
-
-        } else {
-
-            texto.textContent--;
-
-            curtiu = false;
-
         }
 
     });
 
 });
 
-
-// BOTÃO DE ALTERAR O TEMA
 
 const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
 
